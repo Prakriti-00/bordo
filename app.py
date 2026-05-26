7c1c5a51a6ee9b29322bf5c4bd4e5f6c1541d70d
Prakriti <prakriti.raysad@gmail.com>
2026-05-26 20:33:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ init_db()
 from db import add_user
 
 #add user here - add_user("username")
+add_user("prakriti")
+add_user("prashant")
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
